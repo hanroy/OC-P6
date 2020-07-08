3f264df1ffd9e33f606e1250ba5b76f4b415fb27
Han Roy <28631866+hanroy@users.noreply.github.com>
2020-07-08 22:00:03 +0200

    add argparse to call json file as arg

--- wp_backup.py
+++ wp_backup.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
+#coding: utf-8
 
 """
 ============================================
         Automate wordpress backup
             Powered by Python
+            Author : Hanen Royer
 ============================================
 """
 
@@ -19,44 +21,72 @@ from subprocess import Popen, PIPE
 from datetime import date
 import time
 import json
+import argparse
 import shutil
 import logging as lg
+
+try:
+    import tqdm
+except ImportError:
+    print ("Trying to Install required module: tqdm")
+    os.system('pip3 install tqdm')
+from tqdm import tqdm
+
+
+try:
+    import ftplib
+except ImportError:
+    print ("Trying to Install required module: ftplib")
+    os.system('pip3 install ftplib')
 from ftplib import FTP_TLS, error_perm
 
-lg.basicConfig(level=lg.DEBUG)
+#######################################################
+# Log to both logfile and console                        #
+#######################################################
 
-today = date.today()
-d1 = today.strftime("%m-%d-%Y")
+lg.basicConfig(filename="/root/backup-wp/log/log.txt", filemode='a', format='%(asctime)s : %(levelname)s : %(message)s', datefmt='%d/%m/%Y %H:%M', level=lg.DEBUG)
+console = lg.StreamHandler()
+console.setLevel(lg.INFO)
+formatter = lg.Formatter('%(message)s')
+console.setFormatter(formatter)
+lg.getLogger('').addHandler(console)
 
 
 #######################################################
 # variables                                           #
 #######################################################
 
-with open("/root/backup-wp/variables.json", "r") as f:
-    my_dict = json.load(f)
+today = date.today()
+d1 = date.today().strftime("%m-%d-%Y")
 
-wp_backup_path = (my_dict["wp_bkp_local_path"]) +d1   #set wordpress backup folder
-days = int((my_dict["days"])) #set days
-folder = (my_dict["folder"]) +d1 #set wordpress folder name
-backuppath = (my_dict["wp_bkp_path"]) #set wordpress path location
-ftp_ip = (my_dict["ftp_ip"]) #set ftp IP
-ftp_port = int((my_dict["ftp_port"])) #set ftp port
-ftp_login = (my_dict["ftp_login"]) #set ftp login
-ftp_password = (my_dict["ftp_password"]) #set ftp password
-shell_script = (my_dict["shell_script"])
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", required=True, type=str)
+args = parser.parse_args()
+
+with open(args.input) as json_data:
+    try:
+        data = json.load(json_data)
+    except json.JSONDecodeError:
+        print('Invalid JSON format')
+
+
+wp_backup_path = (data["wp_bkp_local_path"]) +d1   #set wordpress backup folder
+days = int((data["days"])) #set days
+folder = (data["folder"]) +d1 #set wordpress folder name
+backuppath = (data["wp_bkp_path"]) #set wordpress path location
+ftp_ip = (data["ftp_ip"]) #set ftp IP
+ftp_port = int((data["ftp_port"])) #set ftp port
+ftp_login = (data["ftp_login"]) #set ftp login
+ftp_password = (data["ftp_password"]) #set ftp password
+shell_script = (data["shell_script"])
+
 
 #######################################################
 # Wordpress clean backup folder                       #
 # Delete folder older than 5 days                     #
 #######################################################
 
-
-"""
-============================================
-    	Automate wordpress backup
-============================================
-"""
+lg.info(f"Delete wordpress backup folder older than {days} days")
 
 def main():
 
@@ -127,6 +157,9 @@ if __name__ == '__main__':
 # Wordpress create backup folder                      #
 #######################################################
 
+lg.info('Create wordpress backup folder with today date')
+
+
 def create_wp_bkp_folder():
     CHECK_FOLDER = os.path.isdir(wp_backup_path)
     if not CHECK_FOLDER:
@@ -148,10 +181,11 @@ create_wp_bkp_folder()
 ## backup wordpress folder and compress               #
 #######################################################
 
+lg.info('Backup wordpress database and compress wordpress path with today date')
 def call_shell_backupbdd():
     try:
         file = open(shell_script, 'r')
-        lg.info(f'{file} exist!')
+        lg.info(f'{shell_script} exist!')
         session = subprocess.Popen([shell_script], stdout=PIPE, stderr=PIPE)
         lg.info('The backup is successfully executed')
         stdout, stderr = session.communicate(input="Hello from the other side!")
@@ -170,13 +204,7 @@ call_shell_backupbdd()
 # Send wordpress backup folder to ftp server          #
 #######################################################
 
-try:
-    import ftplib
-except:
-    print()
-    print("ftplib , module Not Found")
-    print("Install: pip3 install ftplib")
-    sys.exit(1)
+lg.info('Send wordpress backup folder to FTP server')
 
 try:
     ftps = FTP_TLS(timeout=100)
@@ -185,6 +213,7 @@ try:
     ftps.prot_p()
     ftps.set_pasv(True)
     ftps.login(ftp_login, ftp_password)
+    print('Connect to FTP ...')
     print(ftps.getwelcome())
 
 
@@ -193,27 +222,35 @@ except ftplib.error_perm as e:
 
 ftps.nlst()
 files_list = ftps.nlst()
-print("List of remote folders:")
+lg.info("List of FTP remote folders:")
 for filename in files_list:
     lg.debug(f'{filename} ')
 
 
 if folder in ftps.nlst() : #check if 'wordpress-todayDate' exist inside '/home/wordpress'
     ftps.cwd(folder) #change into "wordpress-todayDate" directory
+    lg.debug(f'{folder} ')
 else :
     ftps.mkd(folder) #Create a new directory called "wordpress-todayDate" on the server.
+    lg.debug(f'{folder} 2')
     ftps.cwd(folder)
 
 def placeFiles():
     for file in os.listdir(wp_backup_path):
         localpath = os.path.join(wp_backup_path, file)
         if os.path.isfile(localpath):
-            print("STOR", file, localpath)
-            ftps.storbinary('STOR ' + file, open(localpath,'rb'))
+            filesize = os.path.getsize(localpath)
+            #print("STOR", file, localpath)
+            with tqdm(unit = 'blocks', unit_scale = True, leave = False, miniters = 1, desc = 'Uploading Files......', total = filesize) as tqdm_instance:
+                ftps.storbinary('STOR ' + file, open(localpath,'rb'), 128, callback = lambda sent: tqdm_instance.update(len(sent)))
 
+#print('Send Wordpress backup folder in progress ..')
+lg.info('Send Wordpress backup folder in progress ..')
 placeFiles()
-
+lg.debug('Wordpress backup folder successfully sent ')
+#print('Quit FTP connection')
 ftps.quit()
-print('Closing FTP connection')
+lg.info('Closing FTP connection')
 ftps.close() #close connection
+
 
